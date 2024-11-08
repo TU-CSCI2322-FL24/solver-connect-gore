b57ab9e28c97679ab9ecd145fff4f698b871a4b1
Aidan <aidanmcloughlinatx@gmail.com>
2024-11-08 13:25:23 -0600

    created a general plan for story_5

--- Solver.hs
+++ Solver.hs
@@ -4,3 +4,9 @@ type Board = [[Color]]
 data Color = Red | Yellow
 type Move = Int
 data Winner = Won Color | Tie | Ongoing
+-- make a function that turns the board from a [[Color]] columns into [[Maybe Color]] rows
+-- built in transpose funtion for haskell which can rotate the board
+-- for printing it makes more sense to have the horizontal rows than vertical columns
+
+
+
