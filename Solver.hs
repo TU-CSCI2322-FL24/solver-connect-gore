9f5b74406442b7c3516e2203ebcec2679a34285d
Eheath04 <eheath@trinity.edu>
2024-11-11 13:09:20 -0600

    Commit branch 4 with validMoves

--- Solver.hs
+++ Solver.hs
@@ -1,6 +1,18 @@
 --implementation option 1
 type Game  = (Color,Board)
 type Board = [[Color]]
-data Color = Red | Yellow
-type Move = Int
-data Winner = Won Color | Tie | Ongoing
+data Color = Red | Yellow deriving (Show)
+type Move = Int 
+data Winner = Won Color | Tie | Ongoing deriving (Show,Eq)
+
+position :: Board -> Int ->[Move] -> [Move]
+position [] index acc = acc
+position (x:xs) index acc  
+    |length x == 6 =  position xs (index+1) acc
+    |otherwise     = position xs (index +1) (index:acc)
+    
+
+
+validMoves :: Game -> [Move]
+validMoves (_,board) =reverse( position board 0 [])
+
