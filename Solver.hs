467e80205b4471d94e4c40517b0874f06d1b8422
Aidan <aidanmcloughlinatx@gmail.com>
2024-12-04 13:23:07 -0600

    finished the count function

--- Solver.hs
+++ Solver.hs
@@ -6,7 +6,7 @@ type Board = [[Color]]
 data Color = Red | Yellow deriving (Show, Eq)
 type Move = Int
 data Winner = Won Color | Tie deriving (Show, Eq)
-
+type Rating = Int
 
 -- 
 -- Story 2
@@ -357,47 +357,68 @@ main =
 -- subtract a point for every piece that is of the opponent's color 
 rateGame :: Game -> Rating
 rateGame game@(curr,brd) = undefined
-  let winnerLst = checkVertical brd ++ (checkHorizontal brd) ++ (checkDiagonal brd)
-  in if winnerLst == [] 
-      then if validMoves game == [] 
-            then Just Tie
-            else Nothing
-      else if Red `elem` winnerLst
-            then if Yellow `elem` winnerLst 
-                  then Just Tie
-                  else Just (Won Red)
-            else Just (Won Yellow)
+  
+  | Red `elem` list && Yellow `elem` list = 0
+  | otherwise                             = count Red list - count Yellow list
 
+count :: Color -> [Color] -> Rating
+count color [] = 0
+count color (x:xs) = 
+  if x == color 
+    then 1 + count color xs
+    else count color xs
 
+rateVertical :: 
+rateVertical =
+brd@(col@():rest)
+
+  | Red `elem` list && Yellow `elem` list = 0
+  | otherwise                             = count Red list - count Yellow list
 
 
 
 checkFour (Red:_) (Red:_) (Red:_) (Red:_) = 4 
 checkFour (Yellow:_) (Yellow:_) (Yellow:_) (Yellow:_) = -4
 checkFour (_:xs) (_:ys) (_:zs) (_:ws) = 0 + checkFour xs ys zs ws
-checkFour _ _ _ _ = []
+checkFour _ _ _ _ = 0
 
-checkHorizontal (xs:ys:zs:ws:rest) = checkFour xs ys zs ws ++ checkHorizontal (ys:zs:ws:rest)
-checkHorizontal _ = []
+checkHorizontal (xs:ys:zs:ws:rest) = checkFour xs ys zs ws + checkHorizontal (ys:zs:ws:rest)
+checkHorizontal _ = 0
 
--- Checks one column for a win, returning a list of winning colors
-checkColumn col =
-  let aux 4 clr _ = [clr]
-      aux _ _ [] = []
-      aux i clr (x:xs) =
-        if x == clr then aux (i+1) x xs
-        else aux 1 x xs
-  in if col == [] then []
-     else aux 1 (head col) (tail col)
+checkVertical (y@(x,xs):ys) = 
 
 -- Checks all columns for a win, returning a list of winning colors
 checkVertical brd = concat (map checkColumn brd)
+  where checkColumn col =
+          let aux 4 clr _ = [clr]
+              aux _ _ [] = []
+              aux i clr (x:xs) =
+                if x == clr 
+                  then aux (i+1) x xs
+                  else aux 1 x xs
+          in if col == [] 
+              then []
+              else aux 1 (head col) (tail col)
 
 checkDiags (xs:ys:zs:ws:rest) =
   checkFour xs (drop 1 ys) (drop 2 zx) (drop 3 ws)
   ++ checkfour (drop 3 xs) ( check 2 ys) (drop 1 zs) ws
   ++ checkDiags (ys:zs:ws:rest)
 
+
+
+
+
+  -- let winnerLst = checkVertical brd ++ (checkHorizontal brd) ++ (checkDiagonal brd)
+  -- in if winnerLst == [] 
+  --     then if validMoves game == [] 
+  --           then Just Tie
+  --           else Nothing
+  --     else if Red `elem` winnerLst
+  --           then if Yellow `elem` winnerLst 
+  --                 then Just Tie
+  --                 else Just (Won Red)
+  --           else Just (Won Yellow)
 
 -- 
 -- End of Story 17
